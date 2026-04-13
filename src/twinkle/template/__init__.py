29690c7f3e52ef547b630109aa97e093221f1bd2
a550580874 <82751568+a550580874@users.noreply.github.com>
2026-04-13 16:37:06 +0800

    add qwen3 next text template

--- src/twinkle/template/__init__.py
+++ src/twinkle/template/__init__.py
@@ -1,3 +1,4 @@
 # Copyright (c) ModelScope Contributors. All rights reserved.
 from .base import Template
+from .qwen3_next_text import Qwen3NextTextTemplate
 from .qwen3_5_vl import Qwen3_5Template
---
+++ src/twinkle/template/qwen3_next_text.py
@@ -0,0 +1,10 @@
+from twinkle import remote_class
+
+from .base import Template
+
+
+@remote_class()
+class Qwen3NextTextTemplate(Template):
+    """Minimal text-only template for Qwen3-Coder-Next style models."""
+
+    pass
