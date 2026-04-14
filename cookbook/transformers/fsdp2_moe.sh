d1792dbad20b492bf052c137fe3126682a59dd2d
a550580874 <82751568+a550580874@users.noreply.github.com>
2026-04-14 09:30:17 +0800

    Update fsdp2_moe.sh with default values and proxy settings
    Set default model reference and template model ID. Added environment variable exports for proxy settings.

--- cookbook/transformers/fsdp2_moe.sh
+++ cookbook/transformers/fsdp2_moe.sh
@@ -1,4 +1,9 @@
-MODEL_REF="${1:-/data/weight/Qwen3-Coder-Next/}"
-TEMPLATE_MODEL_ID="${2:-${MODEL_REF}}"
+MODEL_REF="/data/weight/Qwen3-Coder-Next/"
+TEMPLATE_MODEL_ID="/data/weight/Qwen3-Coder-Next/"
+source /usr/local/Ascend/ascend-toolkit/set_env.sh 
+IP=
+export http_proxy="http://p_atlas:proxy%40123@$IP:8080"
+export https_proxy="http://p_atlas:proxy%40123@$IP:8080"
+export no_proxy=127.0.0.1,.huawei.com,localhost,local,.local
 
 CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 torchrun --nproc_per_node=8 fsdp2_moe.py --model-ref "${MODEL_REF}" --template-model-id "${TEMPLATE_MODEL_ID}"
