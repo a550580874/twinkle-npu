9e6f68249f0b2bc349fca4cb26d1a3163a9b0d0b
a550580874 <82751568+a550580874@users.noreply.github.com>
2026-04-14 09:28:30 +0800

    update fsdp2 moe for next coder

--- cookbook/transformers/fsdp2_moe.sh
+++ cookbook/transformers/fsdp2_moe.sh
@@ -1,1 +1,4 @@
-CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 torchrun --nproc_per_node=8 fsdp2_moe.py
+MODEL_REF="${1:-/data/weight/Qwen3-Coder-Next/}"
+TEMPLATE_MODEL_ID="${2:-${MODEL_REF}}"
+
+CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 torchrun --nproc_per_node=8 fsdp2_moe.py --model-ref "${MODEL_REF}" --template-model-id "${TEMPLATE_MODEL_ID}"
