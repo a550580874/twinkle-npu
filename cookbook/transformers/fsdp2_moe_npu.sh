c5e3c73541b21a01ec8fe588070a391ef9c4e723
a550580874 <82751568+a550580874@users.noreply.github.com>
2026-04-17 14:34:50 +0800

    Rename fsdp2_moe.sh and update script commands

--- cookbook/transformers/fsdp2_moe_npu.sh
+++
@@ -1,9 +0,0 @@
-MODEL_REF="/data/weight/Qwen3-Coder-Next/"
-TEMPLATE_MODEL_ID="/data/weight/Qwen3-Coder-Next/"
-source /usr/local/Ascend/ascend-toolkit/set_env.sh 
-IP=
-export http_proxy="http://p_atlas:proxy%40123@$IP:8080"
-export https_proxy="http://p_atlas:proxy%40123@$IP:8080"
-export no_proxy=127.0.0.1,.huawei.com,localhost,local,.local
-
-ASCEND_RT_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 torchrun --nproc_per_node=8 fsdp2.py --model-ref "${MODEL_REF}" --template-model-id "${TEMPLATE_MODEL_ID}"
